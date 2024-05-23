d920aacff0f5a78548c5b74f6c36f57179baa9c8
tarun <tarunrk2002@gmail.com>
2024-05-23 11:56:03 +0530

    Add project files.

---
+++ dependency_Injection_service_lifetime/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dependency_Injection_service_lifetime.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
